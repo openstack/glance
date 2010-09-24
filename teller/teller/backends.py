7035db08fa8f07f52ee7329bc3e436081b4e0fd0
jaypipes@gmail.com <>
2010-09-24 16:36:31 -0400

    Implements Swift backend for teller

--- teller/teller/backends.py
+++ teller/teller/backends.py
@@ -1,12 +1,37 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010 OpenStack, LLC
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import cloudfiles
+import httplib
+import re
 import urlparse
+
 
 class BackendException(Exception):
     pass
+
+
 class UnsupportedBackend(BackendException):
     pass
 
+
 class Backend(object):
     CHUNKSIZE = 4096
+
 
 class TestStrBackend(Backend):
     @classmethod
@@ -15,6 +40,7 @@ class TestStrBackend(Backend):
         teststr://data
         """
         yield parsed_uri.netloc
+
 
 class FilesystemBackend(Backend):
     @classmethod
@@ -32,6 +58,7 @@ class FilesystemBackend(Backend):
                 yield chunk
                 chunk = f.read(cls.CHUNKSIZE)
          
+
 class HTTPBackend(Backend):
     @classmethod
     def get(cls, parsed_uri, conn_class=None):
@@ -39,7 +66,6 @@ class HTTPBackend(Backend):
         http://netloc/path/to/file.tar.gz.0
         https://netloc/path/to/file.tar.gz.0
         """
-        import httplib
         if conn_class:
             pass # use the conn_class passed in
         elif parsed_uri.scheme == "http":
@@ -47,7 +73,7 @@ class HTTPBackend(Backend):
         elif parsed_uri.scheme == "https":
             conn_class = httplib.HTTPSConnection
         else:
-            raise BackendException("scheme '%s' not support for HTTPBackend")
+            raise BackendException("scheme '%s' not supported for HTTPBackend")
         conn = conn_class(parsed_uri.netloc)
         conn.request("GET", parsed_uri.path, "", {})
         try:
@@ -59,13 +85,56 @@ class HTTPBackend(Backend):
         finally:
             conn.close()
 
+class SwiftBackend(Backend):
+    """
+    An implementation of the swift backend adapter.
+    """
+
+    RE_SWIFT_TOKENS = re.compile(r":|@|/")
+    EXAMPLE_URL="swift://user:password@auth_url/container/file.gz.0"
+
+    @classmethod
+    def get(cls, parsed_uri, conn_class=None):
+        """
+        Takes a parsed_uri in the format of: 
+        swift://user:password@auth_url/container/file.gz.0, connects to the 
+        swift instance at auth_url and downloads the file. Returns the generator
+        provided by stream() on the swift object representing the file.
+        """
+        if conn_class:
+            pass # Use the provided conn_class
+        else:
+            conn_class = cloudfiles
+
+        try:
+            split_url = parsed_uri.path[2:]
+            swift_tokens = cls.RE_SWIFT_TOKENS.split(split_url)
+            user, api_key, authurl, container, file = swift_tokens
+        except ValueError:
+            raise BackendException(
+                 "Expected four values to unpack in: swift:%s. "
+                 "Should have received something like: %s."
+                 % (parsed_uri.path, cls.EXAMPLE_URL))
+
+        swift_conn = conn_class.get_connection(username=user, api_key=api_key,
+                                               authurl=authurl)
+
+        container = swift_conn.get_container(container)
+        obj = container.get_object(file)
+
+        # Return the generator provided from obj.stream()
+        return obj.stream(chunksize=cls.CHUNKSIZE)
+
+
 def _scheme2backend(scheme):
     return {
         "file": FilesystemBackend,
         "http": HTTPBackend,
         "https": HTTPBackend,
+        "swift": SwiftBackend,
         "teststr": TestStrBackend
     }[scheme]
+
 
 def get_from_backend(uri, **kwargs):
     """
--- teller/tests/unit/test_backends.py
+++ teller/tests/unit/test_backends.py
@@ -1,6 +1,29 @@
-import unittest
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010 OpenStack, LLC
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 from StringIO import StringIO
-from teller.backends import Backend, get_from_backend
+import unittest
+
+from cloudfiles import Connection
+from cloudfiles.authentication import MockAuthentication as Auth
+
+from swiftfakehttp import CustomHTTPConnection
+from teller.backends import Backend, BackendException, get_from_backend
+
 
 class TestBackends(unittest.TestCase):
     def setUp(self):
@@ -35,6 +58,42 @@ class TestBackends(unittest.TestCase):
 
         chunks = [c for c in fetcher]
         self.assertEqual(chunks, ["fa", "ke", "da", "ta"])
+
+    def test_swift_get_from_backend(self):
+        class FakeSwift(object):
+            def __init__(self, *args, **kwargs): 
+                pass
+            @classmethod
+            def get_connection(self, *args, **kwargs):
+                auth = Auth("user", "password")
+                conn = Connection(auth=auth)
+                conn.connection = CustomHTTPConnection("localhost", 8000)
+                return conn
+
+        swift_returns = ['I ', 'am', ' a', ' t', 'ea', 'po', 't,', ' s', 'ho', 'rt', ' a', 'nd', ' s', 'to', 'ut', '\n']
+
+        fetcher = get_from_backend("swift://user:password@localhost/container1/file.tar.gz",
+                                  conn_class=FakeSwift)
+
+        chunks = [c for c in fetcher]
+
+        self.assertEqual(chunks, swift_returns)
+
+    def test_swift_get_from_backend_with_bad_uri(self):
+        class FakeSwift(object):
+            def __init__(self, *args, **kwargs): 
+                pass
+            @classmethod
+            def get_connection(self, *args, **kwargs):
+                auth = Auth("user", "password")
+                conn = Connection(auth=auth)
+                conn.connection = CustomHTTPConnection("localhost", 8000)
+                return conn
+
+        swift_url="swift://localhost/container1/file.tar.gz"
+
+        self.assertRaises(BackendException, get_from_backend, swift_url)
+
 
 if __name__ == "__main__":
     unittest.main()
