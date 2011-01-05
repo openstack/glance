71650ebbbaaf50713542f8acc4a33076ae730933
Christopher MacGown <chris@slicehost.com>
2011-01-05 10:56:58 +0100

    Fixed pylint/pep8 for glance.store.s3

--- glance/store/s3.py
+++ glance/store/s3.py
@@ -30,13 +30,13 @@ class S3Backend(glance.store.Backend):
     @classmethod
     def get(cls, parsed_uri, expected_size, conn_class=None):
         """
-        Takes a parsed_uri in the format of: 
-        s3://access_key:secret_key@s3.amazonaws.com/bucket/file.gz.0, connects to s3
-        and downloads the file. Returns the generator resp_body provided by 
-        get_object.
+        Takes a parsed_uri in the format of:
+        s3://access_key:secret_key@s3.amazonaws.com/bucket/file.gz.0, connects
+        to s3 and downloads the file. Returns the generator resp_body provided
+        by get_object.
         """
 
-        if conn_class: 
+        if conn_class:
             pass
         else:
             conn_class = boto.s3.connection.S3Connection
@@ -51,8 +51,9 @@ class S3Backend(glance.store.Backend):
             # Close the key when we're through.
             with cls._get_key(bucket, obj) as key:
                 if not key.size == expected_size:
-                    raise glance.store.BackendException("Expected %s bytes, got %s"
-                        % (expected_size, key.size))
+                    raise glance.store.BackendException(
+                        "Expected %s bytes, got %s" %
+                        (expected_size, key.size))
 
                 key.BufferSize = cls.CHUNKSIZE
                 for chunk in key:
@@ -61,12 +62,13 @@ class S3Backend(glance.store.Backend):
     @classmethod
     def delete(cls, parsed_uri, conn_class=None):
         """
-        Takes a parsed_uri in the format of: 
-        s3://access_key:secret_key@s3.amazonaws.com/bucket/file.gz.0, connects to s3
-        and deletes the file. Returns whatever boto.s3.key.Key.delete() returns.
+        Takes a parsed_uri in the format of:
+        s3://access_key:secret_key@s3.amazonaws.com/bucket/file.gz.0, connects
+        to s3 and deletes the file. Returns whatever boto.s3.key.Key.delete()
+        returns.
         """
 
-        if conn_class: 
+        if conn_class:
             pass
         else:
             conn_class = boto.s3.connection.S3Connection
@@ -82,15 +84,13 @@ class S3Backend(glance.store.Backend):
             with cls._get_key(bucket, obj) as key:
                 return key.delete()
 
-
-
     @classmethod
     def _get_bucket(cls, conn, bucket_id):
         """ Get a bucket from an s3 connection """
 
         bucket = conn.get_bucket(bucket_id)
         if not bucket:
-            raise glance.store.BackendException("Could not find bucket: %s" % 
+            raise glance.store.BackendException("Could not find bucket: %s" %
                 bucket_id)
 
         return bucket
