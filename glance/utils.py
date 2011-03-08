6ab6bcfd347f49a5d9d080353bb0edce5e3eec58
Vishvananda Ishaya <vishvananda@gmail.com>
2011-03-08 20:12:41 +0000

    fix data passing

--- glance/utils.py
+++ glance/utils.py
@@ -32,8 +32,12 @@ def image_meta_to_http_headers(image_meta):
     for k, v in image_meta.items():
         if k == 'properties':
             for pk, pv in v.items():
+                if pv is None:
+                    pv = ''
                 headers["x-image-meta-property-%s"
                         % pk.lower()] = unicode(pv)
+        if v is None:
+            v = ''
         headers["x-image-meta-%s" % k.lower()] = unicode(v)
     return headers
 
@@ -76,10 +80,10 @@ def get_image_meta_from_headers(response):
         key = str(key.lower())
         if key.startswith('x-image-meta-property-'):
             field_name = key[len('x-image-meta-property-'):].replace('-', '_')
-            properties[field_name] = value
+            properties[field_name] = value or None
         elif key.startswith('x-image-meta-'):
             field_name = key[len('x-image-meta-'):].replace('-', '_')
-            result[field_name] = value
+            result[field_name] = value or None
     result['properties'] = properties
     if 'id' in result:
         result['id'] = int(result['id'])
@@ -87,6 +91,8 @@ def get_image_meta_from_headers(response):
         result['size'] = int(result['size'])
     if 'is_public' in result:
         result['is_public'] = (result['is_public'] == 'True')
+    if 'deleted' in result:
+        result['deleted'] = (result['deleted'] == 'True')
     return result
 
 
