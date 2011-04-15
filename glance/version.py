772658fa4ab98786af5df180ec944b56c88ec379
Thierry Carrez <thierry@openstack.org>
2011-04-15 11:47:55 +0200

    Diablo versioning

--- glance/version.py
+++ glance/version.py
@@ -21,10 +21,10 @@ except ImportError:
                     'revision_id': 'LOCALREVISION',
                     'revno': 0}
 
-GLANCE_VERSION = ['2011', '2']
+GLANCE_VERSION = ['2011', '3']
 YEAR, COUNT = GLANCE_VERSION
 
-FINAL = True   # This becomes true at Release Candidate time
+FINAL = False   # This becomes true at Release Candidate time
 
 
 def canonical_version_string():
