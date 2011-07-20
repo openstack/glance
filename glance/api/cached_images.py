8094e74f5d16f169f7c72bb7f48da4e4835492c2
Rick Harris <rconradharris@gmail.com>
2011-07-20 16:58:59 -0500

    Initial work on moving cached_images to WSGI middleware

---
+++ glance/api/cached_images.py
@@ -0,0 +1,63 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2011 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Controller that returns information on the Glance API versions
+"""
+
+import httplib
+import json
+
+import webob.dec
+
+from glance.common import wsgi
+
+
+class Controller(object):
+
+    """
+    A controller that produces information on the Glance API versions.
+    """
+
+    def __init__(self, options):
+        self.options = options
+
+    def index(self, req):
+        images = [dict(id=123, name='blah')]
+        return dict(images=images)
+
+
+class CachedImageDeserializer(wsgi.JSONRequestDeserializer):
+    pass
+
+
+class CachedImageSerializer(wsgi.JSONResponseSerializer):
+    pass
+
+
+def create_resource(options):
+    """Cached Images resource factory method"""
+    deserializer = CachedImageDeserializer()
+    serializer = CachedImageSerializer()
+    return wsgi.Resource(Controller(options), deserializer, serializer)
+
+
+def app_factory(global_conf, **local_conf):
+    """paste.deploy app factory for creating Cached Images apps"""
+    conf = global_conf.copy()
+    conf.update(local_conf)
+    return Controller(conf)
