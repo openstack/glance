486deeabd695bfd06a2aaf3c047e7206cd39f61f
Justin Shepherd <jshepher@rackspace.com>
2011-08-04 11:00:00 -0500

    Implemented @utils.skip_test, @utils.skip_unless and @utils.skip_if functionality in glance/test/utils.py. Added glance/tests/unit/test_skip_examples.py which contains example skip case usages.
    Fixed issue where ./run_tests.sh would not execute pep8. Fixed couple of pep8 violations in test_skip_examples.py
    
    Change-Id: Id6eaa8768b663b4638fbca0e3bdf72b74969150a

--- run_tests.sh
+++ run_tests.sh
@@ -59,7 +59,7 @@ function run_pep8 {
   PEP8_EXCLUDE=vcsversion.py
   PEP8_OPTIONS="--exclude=$PEP8_EXCLUDE --repeat --show-pep8 --show-source"
   PEP8_INCLUDE="bin/* glance tools setup.py run_tests.py"
-  pep8 $PEP8_OPTIONS $PEP8_INCLUDE
+  ${wrapper} pep8 $PEP8_OPTIONS $PEP8_INCLUDE
 }
 
 
