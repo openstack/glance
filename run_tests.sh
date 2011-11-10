6eb28314591382366bb07e32b856420fc6cc82f2
Monty Taylor <mordred@inaugust.com>
2011-11-10 12:08:39 -0500

    Removed some cruft.
    Removed builddeb.sh - we don't use it.
    Removed a reference to pep8 skipping vcsversion due to bzr bug.
    Updated sqlalchemy in the pip-requires to allow/use 0.7.1 or later to match
    Nova installs.
    
    Change-Id: I2da2b39efd0b1b192a490509a44cb1f641b86092

--- run_tests.sh
+++ run_tests.sh
@@ -50,13 +50,6 @@ function run_tests {
 
 function run_pep8 {
   echo "Running pep8 ..."
-  # FIXME(sirp): bzr version-info is not currently pep-8. This was fixed with
-  # lp701898 [1], however, until that version of bzr becomes standard, I'm just
-  # excluding the vcsversion.py file
-  #
-  # [1] https://bugs.launchpad.net/bzr/+bug/701898
-  #
-  PEP8_EXCLUDE=vcsversion.py
   PEP8_OPTIONS="--exclude=$PEP8_EXCLUDE --repeat --show-pep8 --show-source"
   PEP8_INCLUDE="bin/* glance tools setup.py run_tests.py"
   ${wrapper} pep8 $PEP8_OPTIONS $PEP8_INCLUDE
