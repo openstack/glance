f8ae3c4995d8716c46750e3715532e3086a2645a
Russell Bryant <rbryant@redhat.com>
2012-01-04 16:01:13 -0500

    Split notification strategies out into modules.
    1) Add a Strategy class that defines the interface that a particular
    notification strategy must implement.
    
    2) Split each notification strategy out into its own module.
    
    This is in part just an attempt at some "code cleanup", but it also has
    an advantage that it is easier to make a particular notification
    strategy be optional and not impose a hard dependency on all of glance.
    
    Change-Id: Iab658637c56e764e2b3ec54c67e752db0cbfbe5c

---
+++ glance/notifier/notify_log.py
@@ -0,0 +1,36 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2011, OpenStack LLC.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import logging
+
+from glance.notifier import strategy
+
+
+class LoggingStrategy(strategy.Strategy):
+    """A notifier that calls logging when called."""
+
+    def __init__(self, conf):
+        self.logger = logging.getLogger('glance.notifier.logging_notifier')
+
+    def warn(self, msg):
+        self.logger.warn(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
