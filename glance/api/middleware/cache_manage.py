d759821b287398f1422af862abaf133ed23e0736
Brian Waldon <bcwaldon@gmail.com>
2012-03-15 14:14:14 -0700

    Remove dependency on apiv1app from cachemanage
    * Cache management middleware shouldn't depend on being deployed around
      the glance api v1 app. This breaks that dependency in favor of an
      isolated mapper in the cache management middleware.
    * Fixes bug 954596
    
    Change-Id: I3f4f56eac285163d1ce9c7dc1cef5178b16eefb6

--- glance/api/middleware/cache_manage.py
+++ glance/api/middleware/cache_manage.py
@@ -21,6 +21,8 @@ Image Cache Management API
 
 import logging
 
+import routes
+
 from glance.api import cached_images
 from glance.common import wsgi
 
@@ -29,43 +31,56 @@ logger = logging.getLogger(__name__)
 
 class CacheManageFilter(wsgi.Middleware):
     def __init__(self, app, conf, **local_conf):
-        map = app.map
+        mapper = routes.Mapper()
         resource = cached_images.create_resource(conf)
 
-        map.connect("/cached_images",
-                    controller=resource,
-                    action="get_cached_images",
-                    conditions=dict(method=["GET"]))
+        mapper.connect("/v1/cached_images",
+                      controller=resource,
+                      action="get_cached_images",
+                      conditions=dict(method=["GET"]))
 
-        map.connect("/cached_images/{image_id}",
-                    controller=resource,
-                    action="delete_cached_image",
-                    conditions=dict(method=["DELETE"]))
+        mapper.connect("/v1/cached_images/{image_id}",
+                      controller=resource,
+                      action="delete_cached_image",
+                      conditions=dict(method=["DELETE"]))
 
-        map.connect("/cached_images",
-                    controller=resource,
-                    action="delete_cached_images",
-                    conditions=dict(method=["DELETE"]))
+        mapper.connect("/v1/cached_images",
+                      controller=resource,
+                      action="delete_cached_images",
+                      conditions=dict(method=["DELETE"]))
 
-        map.connect("/queued_images/{image_id}",
-                    controller=resource,
-                    action="queue_image",
-                    conditions=dict(method=["PUT"]))
+        mapper.connect("/v1/queued_images/{image_id}",
+                      controller=resource,
+                      action="queue_image",
+                      conditions=dict(method=["PUT"]))
 
-        map.connect("/queued_images",
-                    controller=resource,
-                    action="get_queued_images",
-                    conditions=dict(method=["GET"]))
+        mapper.connect("/v1/queued_images",
+                      controller=resource,
+                      action="get_queued_images",
+                      conditions=dict(method=["GET"]))
 
-        map.connect("/queued_images/{image_id}",
-                    controller=resource,
-                    action="delete_queued_image",
-                    conditions=dict(method=["DELETE"]))
+        mapper.connect("/v1/queued_images/{image_id}",
+                      controller=resource,
+                      action="delete_queued_image",
+                      conditions=dict(method=["DELETE"]))
 
-        map.connect("/queued_images",
-                    controller=resource,
-                    action="delete_queued_images",
-                    conditions=dict(method=["DELETE"]))
+        mapper.connect("/v1/queued_images",
+                      controller=resource,
+                      action="delete_queued_images",
+                      conditions=dict(method=["DELETE"]))
+
+        self._mapper = mapper
+        self._resource = resource
 
         logger.info(_("Initialized image cache management middleware"))
         super(CacheManageFilter, self).__init__(app)
+
+    def process_request(self, request):
+        # Map request to our resource object if we can handle it
+        match = self._mapper.match(request.path, request.environ)
+        if match:
+            request.environ['wsgiorg.routing_args'] = (None, match)
+            return self._resource(request)
+        # Pass off downstream if we don't match the request path
+        else:
+            return None
