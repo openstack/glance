openstack/glance

commit 2d36facf14f4eb2742ba46274e04a73b5231aece

Set content_type for messages in Qpid notifier.
Fix bug 963050.

This patch is the Qpid equivalent for a patch that just went in for the
rabbit/kombu notifier.  Glance encodes the message in json format before
passing it in to the notifier.  Explicitly set the content type for the
messages to 'application/json'.

Change-Id: I2f83f51487541b951f3eca0ae4d160a64f5a3db1

glance/notifier/notify_qpid.py
@@ -135,10 +135,16 @@ class QpidStrategy(strategy.Strategy):
         return self.session.sender(address)
 
     def warn(self, msg):
-        self.sender_warn.send(msg)
+        qpid_msg = qpid.messaging.Message(content=msg,
+                                          content_type='application/json')
+        self.sender_warn.send(qpid_msg)
 
     def info(self, msg):
-        self.sender_info.send(msg)
+        qpid_msg = qpid.messaging.Message(content=msg,
+                                          content_type='application/json')
+        self.sender_info.send(qpid_msg)
 
     def error(self, msg):
-        self.sender_error.send(msg)
+        qpid_msg = qpid.messaging.Message(content=msg,
+                                          content_type='application/json')
+        self.sender_error.send(qpid_msg)

glance/tests/unit/test_notifier.py
@@ -15,6 +15,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import json
 import logging
 import unittest
 
@@ -315,6 +316,8 @@ class TestQpidNotifier(unittest.TestCase):
         super(TestQpidNotifier, self).tearDown()
 
     def _test_notify(self, priority):
+        test_msg = json.dumps({'a': 'b'})
+
         self.mock_connection = self.mocker.CreateMock(self.orig_connection)
         self.mock_session = self.mocker.CreateMock(self.orig_session)
         self.mock_sender = self.mocker.CreateMock(self.orig_sender)
@@ -328,18 +331,18 @@ class TestQpidNotifier(unittest.TestCase):
                 '"type": "topic"}, "create": "always"}' % p)
             self.mock_session.sender(expected_address).AndReturn(
                     self.mock_sender)
-        self.mock_sender.send('stuff')
+        self.mock_sender.send(mox.IgnoreArg())
 
         self.mocker.ReplayAll()
 
         conf = utils.TestConfigOpts({"notifier_strategy": "qpid"})
         notifier = self.notify_qpid.QpidStrategy(conf)
-        if priority == "info":
-            notifier.info("stuff")
-        elif priority == "warn":
-            notifier.warn("stuff")
-        elif priority == "error":
-            notifier.error("stuff")
+        if priority == 'info':
+            notifier.info(test_msg)
+        elif priority == 'warn':
+            notifier.warn(test_msg)
+        elif priority == 'error':
+            notifier.error(test_msg)
 
         self.mocker.VerifyAll()
 

commit 5bed23cbc962d3c6503f0ff93e6d1e326efbd49d

Fix content type for qpid notifier.
Fix bug 980872.

This patch fixes a regression I introduced in
2d36facf14f4eb2742ba46274e04a73b5231aece.  In that patch, I adjusted the
content_type for messages sent with the qpid notifier to be
'application/json' to match a change that went into the kombu notifier.
Unfortunately, it's wrong.

I assumed based on the kombu change that notifications were being json
encoded before being passed into the notification driver.  That's not
the case.  The message is a dict.  So, just revert the change to set the
content_type and let Qpid encode the notification as 'amqp/map'.

Change-Id: Iea027409f6200109b97fed93073cff84bc915536

glance/notifier/notify_qpid.py
@@ -135,16 +135,13 @@ class QpidStrategy(strategy.Strategy):
         return self.session.sender(address)
 
     def warn(self, msg):
-        qpid_msg = qpid.messaging.Message(content=msg,
-                                          content_type='application/json')
+        qpid_msg = qpid.messaging.Message(content=msg)
         self.sender_warn.send(qpid_msg)
 
     def info(self, msg):
-        qpid_msg = qpid.messaging.Message(content=msg,
-                                          content_type='application/json')
+        qpid_msg = qpid.messaging.Message(content=msg)
         self.sender_info.send(qpid_msg)
 
     def error(self, msg):
-        qpid_msg = qpid.messaging.Message(content=msg,
-                                          content_type='application/json')
+        qpid_msg = qpid.messaging.Message(content=msg)
         self.sender_error.send(qpid_msg)

glance/tests/unit/test_notifier.py
@@ -316,7 +316,7 @@ class TestQpidNotifier(unittest.TestCase):
         super(TestQpidNotifier, self).tearDown()
 
     def _test_notify(self, priority):
-        test_msg = json.dumps({'a': 'b'})
+        test_msg = {'a': 'b'}
 
         self.mock_connection = self.mocker.CreateMock(self.orig_connection)
         self.mock_session = self.mocker.CreateMock(self.orig_session)