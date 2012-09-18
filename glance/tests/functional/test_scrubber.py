c4173820b0e5836e1af99a428b140647c1db70af
Paul Bourke <paul-david.bourke@hp.com>
2012-09-18 12:40:40 +0100

    Fix scrubber start & not scrubbing when not daemon
    1) Scrubber not starting
    When the scrubber is run from the command line or via cron, it would try
    to call it's app object which resulted in a TypeError.
    
    2) Scrubber not scrubbing
    In the same usage scenario as above, the delete jobs added to the
    starmap are never started.  A slight delay is enough to start these.
    
    Also add a functional test designed to excercise the scrubber when run
    in "regular mode", which covers both these cases.  I've run the test
    successfully with 1000 images, which should be enough to prove the
    sleep(0.1) does the trick.
    
    Fixes bug 1052537.
    
    Change-Id: If29704d8a425194085b59b7dfd265ffaef16302c

--- glance/tests/functional/test_scrubber.py
+++ glance/tests/functional/test_scrubber.py
@@ -21,6 +21,7 @@ from glance.tests import functional
 
 from glance import client
 from glance.registry import client as registry_client
+from glance.tests.utils import execute
 
 
 TEST_IMAGE_DATA = '*' * 5 * 1024
@@ -120,6 +121,57 @@ class TestScrubber(functional.FunctionalTest):
             if False not in deleted:
                 break
 
+        self.assertTrue(recs)
+        for rec in recs:
+            self.assertEqual(rec['status'], 'deleted')
+
+        self.stop_servers()
+
+    def test_scrubber_app(self):
+        """
+        test that the glance-scrubber script runs successfully when not in
+        daemon mode
+        """
+        self.cleanup()
+        self.start_servers(delayed_delete=True, daemon=False)
+
+        client = self._get_client()
+        registry = self._get_registry_client()
+
+        # add some images and ensure it was successful
+        img_ids = []
+        for i in range(0, 3):
+            meta = client.add_image(TEST_IMAGE_META, TEST_IMAGE_DATA)
+            id = meta['id']
+            img_ids.append(id)
+        filters = {'deleted': True, 'is_public': 'none',
+                   'status': 'pending_delete'}
+        recs = registry.get_images_detailed(filters=filters)
+        self.assertFalse(recs)
+
+        # delete those images
+        for img_id in img_ids:
+            client.delete_image(img_id)
+        recs = registry.get_images_detailed(filters=filters)
+        self.assertTrue(recs)
+
+        filters = {'deleted': True, 'is_public': 'none'}
+        recs = registry.get_images_detailed(filters=filters)
+        self.assertTrue(recs)
+        for rec in recs:
+            self.assertEqual(rec['status'], 'pending_delete')
+
+        # wait for the scrub time on the image to pass
+        time.sleep(self.api_server.scrub_time)
+
+        # scrub images and make sure they get deleted
+        cmd = ("bin/glance-scrubber --config-file %s" %
+               self.scrubber_daemon.conf_file_name)
+        exitcode, out, err = execute(cmd, raise_error=False)
+        self.assertEqual(0, exitcode)
+
+        filters = {'deleted': True, 'is_public': 'none'}
+        recs = registry.get_images_detailed(filters=filters)
         self.assertTrue(recs)
         for rec in recs:
             self.assertEqual(rec['status'], 'deleted')
