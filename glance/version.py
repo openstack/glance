98d9928aaf6d437182cea63e3f1c6c48d08015dc
Mark McLoughlin <markmc@redhat.com>
2013-01-31 21:32:51 +0000

    Bump version to 2012.2.4
    Change-Id: I9dab1dc62e41cdb4143a3c8083e65c4b13a22eb4

--- glance/version.py
+++ glance/version.py
@@ -17,6 +17,6 @@
 
 from glance.openstack.common import version as common_version
 
-NEXT_VERSION = '2012.2.3'
+NEXT_VERSION = '2012.2.4'
 version_info = common_version.VersionInfo('glance',
                                           pre_version=NEXT_VERSION)
