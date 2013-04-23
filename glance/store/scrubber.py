545cb15ed0ca105b04b18534cb67e9593171005e
Tom Hancock <tom.hancock@hp.com>
2013-04-23 11:32:06 +0000

    scrubber: dont print URI of image to be deleted
    Print the ID not the URI of the image to to be scrubbed
    as the URI can contain credentials
    
    fixes bug lp# 1171851
    
    Change-Id: Ice2d3d25a23bae31380759b87b50228ba358dcf1

--- glance/store/scrubber.py
+++ glance/store/scrubber.py
@@ -136,7 +136,7 @@ class Scrubber(object):
         if CONF.metadata_encryption_key is not None:
             uri = crypt.urlsafe_decrypt(CONF.metadata_encryption_key, uri)
         try:
-            LOG.debug(_("Deleting %(uri)s") % {'uri': uri})
+            LOG.debug(_("Deleting %(id)s") % {'id': id})
             # Here we create a request context with credentials to support
             # delayed delete when using multi-tenant backend storage
             ctx = context.RequestContext(auth_tok=self.registry.auth_tok,
