580eb6f62168a11061a80f6e95e3bdb77d1d73f6
Flaper Fesp <flaper87@gmail.com>
2013-05-22 15:40:48 +0200

    Encode headers and params
    BaseClient currently fails when non-ascii characters are passed in
    paramas and headers. This patch ensures those params are correctly
    encoded before the request is made.
    
    The patch adds strutils from oslo and updates gettextutils.
    
    Fixes bug: #1182824
    
    (cherry-picked: db33d99af2b16ae81f815c63a6aea851318a2b34)
    
    Conflicts:
    	openstack-common.conf
    
    Change-Id: I12fc2d93b10c43091c1e921be2db1258beb18439

---
+++ glance/tests/unit/common/test_client.py
@@ -0,0 +1,76 @@
+# Copyright 2013 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import httplib
+import StringIO
+
+import mox
+import testtools
+
+from glance.common import client
+from glance.tests import utils
+
+
+class TestClient(testtools.TestCase):
+
+    def setUp(self):
+        super(TestClient, self).setUp()
+        self.mock = mox.Mox()
+        self.mock.StubOutWithMock(httplib.HTTPConnection, 'request')
+        self.mock.StubOutWithMock(httplib.HTTPConnection, 'getresponse')
+
+        self.endpoint = 'example.com'
+        self.client = client.BaseClient(self.endpoint, port=9191,
+                                        auth_tok=u'abc123')
+
+    def tearDown(self):
+        super(TestClient, self).tearDown()
+        self.mock.UnsetStubs()
+
+    def test_http_encoding_headers(self):
+        httplib.HTTPConnection.request(
+            mox.IgnoreArg(),
+            mox.IgnoreArg(),
+            mox.IgnoreArg(),
+            mox.IgnoreArg())
+
+        # Lets fake the response
+        # returned by httplib
+        fake = utils.FakeHTTPResponse(data="Ok")
+        httplib.HTTPConnection.getresponse().AndReturn(fake)
+        self.mock.ReplayAll()
+
+        headers = {"test": u'ni\xf1o'}
+        resp = self.client.do_request('GET', '/v1/images/detail',
+                                      headers=headers)
+        self.assertEqual(resp, fake)
+
+    def test_http_encoding_params(self):
+        httplib.HTTPConnection.request(
+            mox.IgnoreArg(),
+            mox.IgnoreArg(),
+            mox.IgnoreArg(),
+            mox.IgnoreArg())
+
+        # Lets fake the response
+        # returned by httplib
+        fake = utils.FakeHTTPResponse(data="Ok")
+        httplib.HTTPConnection.getresponse().AndReturn(fake)
+        self.mock.ReplayAll()
+
+        params = {"test": u'ni\xf1o'}
+        resp = self.client.do_request('GET', '/v1/images/detail',
+                                      params=params)
+        self.assertEqual(resp, fake)
