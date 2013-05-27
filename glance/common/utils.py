aa749ed6f349cc8964442f54211e2e79922df78a
AmalaBasha <princessbasha@gmail.com>
2013-05-27 17:18:57 +0530

    Fix pydevd module import error
    Introduces a try-except block in 'setup_remote_pydev_debug' that will
    handle the pydevd module import error gracefully.
    
    Implements: blueprint pydevd-module-import
    Change-Id: Ia25b8ef21266ab52dc11c11e9db0bbfa8542f1b9

--- glance/common/utils.py
+++ glance/common/utils.py
@@ -420,23 +420,25 @@ def mutating(func):
 
 
 def setup_remote_pydev_debug(host, port):
+    error_msg = ('Error setting up the debug environment.  Verify that the'
+                 ' option pydev_worker_debug_port is pointing to a valid '
+                 'hostname or IP on which a pydev server is listening on'
+                 ' the port indicated by pydev_worker_debug_port.')
 
-        error_msg = ('Error setting up the debug environment.  Verify that the'
-                     ' option pydev_worker_debug_port is pointing to a valid '
-                     'hostname or IP on which a pydev server is listening on'
-                     ' the port indicated by pydev_worker_debug_port.')
-
+    try:
         try:
             from pydev import pydevd
+        except ImportError:
+            import pydevd
 
-            pydevd.settrace(host,
-                            port=port,
-                            stdoutToServer=True,
-                            stderrToServer=True)
-            return True
-        except:
-            LOG.exception(error_msg)
-            raise
+        pydevd.settrace(host,
+                        port=port,
+                        stdoutToServer=True,
+                        stderrToServer=True)
+        return True
+    except:
+        LOG.exception(error_msg)
+        raise
 
 
 class LazyPluggable(object):
