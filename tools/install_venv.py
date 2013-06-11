d0314c09737ee43cafd09aec6ea490d7982e245e
Monty Taylor <mordred@inaugust.com>
2013-06-11 11:44:40 -0700

    Rename requirements files to standard names.
    Change-Id: Icde92cdff054f8c1efb4a518ab7cf166a8eb791a

--- tools/install_venv.py
+++ tools/install_venv.py
@@ -55,8 +55,8 @@ def print_help():
 def main(argv):
     root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     venv = os.path.join(root, '.venv')
-    pip_requires = os.path.join(root, 'tools', 'pip-requires')
-    test_requires = os.path.join(root, 'tools', 'test-requires')
+    pip_requires = os.path.join(root, 'requirements.txt')
+    test_requires = os.path.join(root, 'test-requirements.txt')
     py_version = "python%s.%s" % (sys.version_info[0], sys.version_info[1])
     project = 'Glance'
     install = install_venv.InstallVenv(root, venv, pip_requires, test_requires,
