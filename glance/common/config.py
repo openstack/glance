66f52c0b0d03362b72f65631af3e562cc999f84a
Chang Bo Guo <guochbo@cn.ibm.com>
2013-11-01 19:53:07 -0700

    Remove unused method setup_logging
    Method setup_logging in glance/common/config.py wasn't used anywhere.
    Similar function is in oslo, and some modules in Glance are using
    function log.setup from oslo, like glance/cmd/manage.py#L123
    
    Closes-Bug: #1248214
    
    Change-Id: Icda26943f02df5e495179868aa8b1f4c4e151803

--- glance/common/config.py
+++ glance/common/config.py
@@ -88,16 +88,6 @@ CONF = cfg.CONF
 CONF.register_opts(paste_deploy_opts, group='paste_deploy')
 CONF.register_opts(common_opts)
 
-CONF.import_opt('verbose', 'glance.openstack.common.log')
-CONF.import_opt('debug', 'glance.openstack.common.log')
-CONF.import_opt('log_dir', 'glance.openstack.common.log')
-CONF.import_opt('log_file', 'glance.openstack.common.log')
-CONF.import_opt('log_config', 'glance.openstack.common.log')
-CONF.import_opt('log_format', 'glance.openstack.common.log')
-CONF.import_opt('log_date_format', 'glance.openstack.common.log')
-CONF.import_opt('use_syslog', 'glance.openstack.common.log')
-CONF.import_opt('syslog_log_facility', 'glance.openstack.common.log')
-
 
 def parse_args(args=None, usage=None, default_config_files=None):
     CONF(args=args,
@@ -110,51 +100,6 @@ def parse_args(args=None, usage=None, default_config_files=None):
 def parse_cache_args(args=None):
     config_files = cfg.find_config_files(project='glance', prog='glance-cache')
     parse_args(args=args, default_config_files=config_files)
-
-
-def setup_logging():
-    """
-    Sets up the logging options for a log with supplied name
-    """
-
-    if CONF.log_config:
-        # Use a logging configuration file for all settings...
-        if os.path.exists(CONF.log_config):
-            logging.config.fileConfig(CONF.log_config)
-            return
-        else:
-            raise RuntimeError("Unable to locate specified logging "
-                               "config file: %s" % CONF.log_config)
-
-    root_logger = logging.root
-    if CONF.debug:
-        root_logger.setLevel(logging.DEBUG)
-    elif CONF.verbose:
-        root_logger.setLevel(logging.INFO)
-    else:
-        root_logger.setLevel(logging.WARNING)
-
-    formatter = logging.Formatter(CONF.log_format, CONF.log_date_format)
-
-    if CONF.use_syslog:
-        try:
-            facility = getattr(logging.handlers.SysLogHandler,
-                               CONF.syslog_log_facility)
-        except AttributeError:
-            raise ValueError(_("Invalid syslog facility"))
-
-        handler = logging.handlers.SysLogHandler(address='/dev/log',
-                                                 facility=facility)
-    elif CONF.log_file:
-        logfile = CONF.log_file
-        if CONF.log_dir:
-            logfile = os.path.join(CONF.log_dir, logfile)
-        handler = logging.handlers.WatchedFileHandler(logfile)
-    else:
-        handler = logging.StreamHandler(sys.stdout)
-
-    handler.setFormatter(formatter)
-    root_logger.addHandler(handler)
 
 
 def _get_deployment_flavor(flavor=None):
