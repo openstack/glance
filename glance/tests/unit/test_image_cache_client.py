0e470ca468870bb605c8171bf169df9406c52e05
Chang Bo Guo <guochbo@cn.ibm.com>
2013-11-30 05:29:52 -0800

    Replace deprecated method assertEquals
    TestCase.assertEquals is deprecated since Python 2.7, so need use new
    method name assertEqual.
    See http://docs.python.org/2/library/unittest.html#deprecated-aliases
    
    Change-Id: Iae9074f2afe2a572b60f6192aa6c871db42360ec

--- glance/tests/unit/test_image_cache_client.py
+++ glance/tests/unit/test_image_cache_client.py
@@ -37,21 +37,21 @@ class CacheClientTestCase(utils.BaseTestCase):
         expected_data = '{"cached_images": "some_images"}'
         self.client.do_request.return_value = \
             utils.FakeHTTPResponse(data=expected_data)
-        self.assertEquals(self.client.get_cached_images(), "some_images")
+        self.assertEqual(self.client.get_cached_images(), "some_images")
         self.client.do_request.assert_called_with("GET", "/cached_images")
 
     def test_get_queued_images(self):
         expected_data = '{"queued_images": "some_images"}'
         self.client.do_request.return_value = \
             utils.FakeHTTPResponse(data=expected_data)
-        self.assertEquals(self.client.get_queued_images(), "some_images")
+        self.assertEqual(self.client.get_queued_images(), "some_images")
         self.client.do_request.assert_called_with("GET", "/queued_images")
 
     def test_delete_all_cached_images(self):
         expected_data = '{"num_deleted": 4}'
         self.client.do_request.return_value = \
             utils.FakeHTTPResponse(data=expected_data)
-        self.assertEquals(self.client.delete_all_cached_images(), 4)
+        self.assertEqual(self.client.delete_all_cached_images(), 4)
         self.client.do_request.assert_called_with("DELETE", "/cached_images")
 
     def test_queue_image_for_caching(self):
@@ -70,7 +70,7 @@ class CacheClientTestCase(utils.BaseTestCase):
         expected_data = '{"num_deleted": 4}'
         self.client.do_request.return_value = \
             utils.FakeHTTPResponse(data=expected_data)
-        self.assertEquals(self.client.delete_all_queued_images(), 4)
+        self.assertEqual(self.client.delete_all_queued_images(), 4)
         self.client.do_request.assert_called_with("DELETE", "/queued_images")
 
 
