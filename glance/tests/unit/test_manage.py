6cf1fad3d7b1b2f1b8f370f4512bddab983374b1
AmalaBasha <amala.alungal@RACKSPACE.COM>
2014-01-29 12:34:32 +0530

    Add unit tests around glance-manage
    Adds unit tests around glance-manage
    
    Implements bp adds-tests-around-glance-manage
    Change-Id: Ia40030ee00b2555e1ee6f57760d2794a45f25325

---
+++ glance/tests/unit/test_manage.py
@@ -0,0 +1,141 @@
+# Copyright 2014 Rackspace Hosting
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import fixtures
+import mock
+import testtools
+
+import glance
+from glance.cmd import manage
+from glance.db import migration as db_migration
+from glance.openstack.common.db.sqlalchemy import migration
+
+
+class TestLegacyManage(testtools.TestCase):
+
+    def setUp(self):
+        super(TestLegacyManage, self).setUp()
+
+        def clear_conf():
+            manage.CONF.reset()
+            manage.CONF.unregister_opt(manage.command_opt)
+        self.addCleanup(clear_conf)
+
+    def _main_test_helper(self, argv, func_name=None, *exp_args):
+        self.useFixture(fixtures.MonkeyPatch('sys.argv', argv))
+        manage.main()
+        func_name.assert_called_once_with(*exp_args)
+
+    def test_legacy_db_version(self):
+        migration.db_version = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db_version'],
+                               glance.openstack.common.db.sqlalchemy.
+                               migration.db_version,
+                               db_migration.MIGRATE_REPO_PATH, 0)
+
+    def test_legacy_db_sync(self):
+        migration.db_sync = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db_sync'],
+                               glance.openstack.common.db.sqlalchemy.
+                               migration.db_sync,
+                               db_migration.MIGRATE_REPO_PATH, None)
+
+    def test_legacy_db_upgrade(self):
+        migration.db_sync = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db_upgrade'],
+                               migration.db_sync,
+                               db_migration.MIGRATE_REPO_PATH, None)
+
+    def test_legacy_db_version_control(self):
+        migration.db_version_control = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db_version_control'],
+                               migration.db_version_control,
+                               db_migration.MIGRATE_REPO_PATH, None)
+
+    def test_legacy_db_sync_version(self):
+        migration.db_sync = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db_sync', '20'],
+                               migration.db_sync,
+                               db_migration.MIGRATE_REPO_PATH, '20')
+
+    def test_legacy_db_upgrade_version(self):
+        migration.db_sync = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db_upgrade', '20'],
+                               migration.db_sync,
+                               db_migration.MIGRATE_REPO_PATH, '20')
+
+    def test_legacy_db_downgrade_version(self):
+        migration.db_sync = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db_downgrade', '20'],
+                               migration.db_sync,
+                               db_migration.MIGRATE_REPO_PATH, '20')
+
+
+class TestManage(testtools.TestCase):
+
+    def setUp(self):
+        super(TestManage, self).setUp()
+
+        def clear_conf():
+            manage.CONF.reset()
+            manage.CONF.unregister_opt(manage.command_opt)
+        self.addCleanup(clear_conf)
+
+    def _main_test_helper(self, argv, func_name=None, *exp_args):
+        self.useFixture(fixtures.MonkeyPatch('sys.argv', argv))
+        manage.main()
+        func_name.assert_called_once_with(*exp_args)
+
+    def test_db_version(self):
+        migration.db_version = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db', 'version'],
+                               migration.db_version,
+                               db_migration.MIGRATE_REPO_PATH, 0)
+
+    def test_db_sync(self):
+        migration.db_sync = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db', 'sync'],
+                               migration.db_sync,
+                               db_migration.MIGRATE_REPO_PATH, None)
+
+    def test_db_upgrade(self):
+        migration.db_sync = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db', 'upgrade'],
+                               migration.db_sync,
+                               db_migration.MIGRATE_REPO_PATH, None)
+
+    def test_db_version_control(self):
+        migration.db_version_control = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db', 'version_control'],
+                               migration.db_version_control,
+                               db_migration.MIGRATE_REPO_PATH, None)
+
+    def test_db_sync_version(self):
+        migration.db_sync = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db', 'sync', '20'],
+                               migration.db_sync,
+                               db_migration.MIGRATE_REPO_PATH, u'20')
+
+    def test_db_upgrade_version(self):
+        migration.db_sync = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db', 'upgrade', '20'],
+                               migration.db_sync,
+                               db_migration.MIGRATE_REPO_PATH, '20')
+
+    def test_db_downgrade_version(self):
+        migration.db_sync = mock.Mock()
+        self._main_test_helper(['glance.cmd.manage', 'db', 'downgrade', '20'],
+                               migration.db_sync,
+                               db_migration.MIGRATE_REPO_PATH, '20')
