d2a81315316a95dbb6888f778e4f207582c626c3
yangxurong <yangxurong@huawei.com>
2014-03-13 10:04:16 +0800

    Do not use __builtin__ in python3
    __builtin__ does not exist in Python 3, use
    six.moves.builtins instead.
    
    Change-Id: If7d2838b0b90ac2d82c4d62c6dfb3c011f937a0b
    closes-bug: #1290234

--- glance/tests/__init__.py
+++ glance/tests/__init__.py
@@ -15,7 +15,7 @@
 
 # See http://code.google.com/p/python-nose/issues/detail?id=373
 # The code below enables tests to work with i18n _() blocks
-import __builtin__
+import six.moves.builtins as __builtin__
 setattr(__builtin__, '_', lambda x: x)
 
 # Set up logging to output debugging
