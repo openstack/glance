3994eeb8624bfe540f69582dd14044d764954abd
Chris Buccella <buccella@linux.vnet.ibm.com>
2014-03-31 04:58:13 +0000

    Add hacking checks
    There have been a handful of changes recently to fix stylistic bugs
    in the unit tests (example: If41a7). The changes themselves were fine,
    but there are two problems:
    
    1) The stylistic changes they made were not listed in HACKING.rst
    2) They were one-time changes; the rules that were violated will
       continue to be violated.
    
    This change updates HACKING.rst to include a few of the stylistic
    rules that have been fixed recently, and adds checks to ensure that
    they will be enforced going forward. This is based on nova's hacking
    checks.
    
    Change-Id: Ic115342605c472f3a5d255aa570ecb60175ca087

--- glance/hacking/checks.py
+++ glance/hacking/checks.py
@@ -12,6 +12,65 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import re
+
+"""
+Guidelines for writing new hacking checks
+
+ - Use only for Glance-specific tests. OpenStack general tests
+   should be submitted to the common 'hacking' module.
+ - Pick numbers in the range G3xx. Find the current test with
+   the highest allocated number and then pick the next value.
+   If nova has an N3xx code for that test, use the same number.
+ - Keep the test method code in the source file ordered based
+   on the G3xx value.
+ - List the new rule in the top level HACKING.rst file
+ - Add test cases for each new rule to glance/tests/test_hacking.py
+
+"""
+
+
+asse_trueinst_re = re.compile(
+    r"(.)*assertTrue\(isinstance\((\w|\.|\'|\"|\[|\])+, "
+    "(\w|\.|\'|\"|\[|\])+\)\)")
+asse_equal_type_re = re.compile(
+    r"(.)*assertEqual\(type\((\w|\.|\'|\"|\[|\])+\), "
+    "(\w|\.|\'|\"|\[|\])+\)")
+asse_equal_end_with_none_re = re.compile(
+    r"(.)*assertEqual\((\w|\.|\'|\"|\[|\])+, None\)")
+asse_equal_start_with_none_re = re.compile(
+    r"(.)*assertEqual\(None, (\w|\.|\'|\"|\[|\])+\)")
+
+
+def assert_true_instance(logical_line):
+    """Check for assertTrue(isinstance(a, b)) sentences
+
+    G316
+    """
+    if asse_trueinst_re.match(logical_line):
+        yield (0, "G316: assertTrue(isinstance(a, b)) sentences not allowed")
+
+
+def assert_equal_type(logical_line):
+    """Check for assertEqual(type(A), B) sentences
+
+    G317
+    """
+    if asse_equal_type_re.match(logical_line):
+        yield (0, "G317: assertEqual(type(A), B) sentences not allowed")
+
+
+def assert_equal_none(logical_line):
+    """Check for assertEqual(A, None) or assertEqual(None, A) sentences
+
+    G318
+    """
+    res = (asse_equal_start_with_none_re.match(logical_line) or
+           asse_equal_end_with_none_re.match(logical_line))
+    if res:
+        yield (0, "G318: assertEqual(A, None) or assertEqual(None, A) "
+               "sentences not allowed")
+
 
 def no_translate_debug_logs(logical_line, filename):
     dirs = [
@@ -33,4 +92,7 @@ def no_translate_debug_logs(logical_line, filename):
 
 
 def factory(register):
+    register(assert_true_instance)
+    register(assert_equal_type)
+    register(assert_equal_none)
     register(no_translate_debug_logs)
