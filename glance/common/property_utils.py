a6f4a9cc504113f880e83b9193364fce55848024
Stuart McLaren <stuart.mclaren@hp.com>
2015-04-20 12:28:33 +0000

    Remove ordereddict from requirements
    Ordereddict was required for py26 support. Since we have
    dropped support for py26 (we are no longer gating on it) we
    should remove it as a requirement and remove references
    to it in the code.
    
    Change-Id: I648edbb5d0b51c003f08f5f5c4ce700128032b38
    Partial-bug: 1445827
    (cherry picked from commit 20ec82f18c368bbc8a89e79d2d6eee2de2c2a9ea)

--- glance/common/property_utils.py
+++ glance/common/property_utils.py
@@ -12,12 +12,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from collections import OrderedDict
 import ConfigParser
 import re
-try:
-    from collections import OrderedDict
-except ImportError:
-    from ordereddict import OrderedDict
 
 from oslo_config import cfg
 from oslo_log import log as logging
