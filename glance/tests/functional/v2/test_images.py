870e2d57bd331c9e630e11ad27b9d331c333d96f
Flavio Percoco <flaper87@gmail.com>
2015-06-09 13:11:36 +0200

    Return empty str for permissive, none, properties
    In Kilo, a patch landed to allow glance for returning None values. While
    this was not an issue for the base and registered properties in the
    `schema-image.json` file, it did break backwards compatibility for the
    custom images created in V1 that have a vulue equal to None.
    
    In order to restore compatibility, this changes returns empty strings
    for those custom properties that exist in the database but that are not
    part of the V2 schema. This strategy will help migrating such properties
    when updates to the image data happens and it'll be a noop for just
    reads.
    
    We can't use a schema migration because we don't know off hand which of
    the properties in the database are the ones that would need to be
    migrated.
    
    While we could skip this properties entirely, this patch prefers to send
    the empty string back as a way to create awareness on the fact that
    there's an empty property in the database. Since we didn't use to return
    these properties, we can assume they weren't being used.
    
    Closes-bug: #1419823
    Change-Id: I59bb27a892fe9485fc98a612ca0148a84123f5a2
    (cherry picked from commit ccea7b68567afb2d5bb825d0cabf247a9526bd93)

--- glance/tests/functional/v2/test_images.py
+++ glance/tests/functional/v2/test_images.py
@@ -68,6 +68,37 @@ class TestImages(functional.FunctionalTest):
         base_headers.update(custom_headers or {})
         return base_headers
 
+    def test_v1_none_properties_v2(self):
+        self.api_server.deployment_flavor = 'noauth'
+        self.api_server.use_user_token = True
+        self.api_server.send_identity_credentials = True
+        self.registry_server.deployment_flavor = ''
+        # Image list should be empty
+        self.start_servers(**self.__dict__.copy())
+
+        # Create an image (with two deployer-defined properties)
+        path = self._url('/v1/images')
+        headers = self._headers({'content-type': 'application/octet-stream'})
+        headers.update(test_utils.minimal_headers('image-1'))
+        # NOTE(flaper87): Sending empty string, the server will use None
+        headers['x-image-meta-property-my_empty_prop'] = ''
+
+        response = requests.post(path, headers=headers)
+        self.assertEqual(201, response.status_code)
+        data = jsonutils.loads(response.text)
+        image_id = data['image']['id']
+
+        # NOTE(flaper87): Get the image using V2 and verify
+        # the returned value for `my_empty_prop` is an empty
+        # string.
+        path = self._url('/v2/images/%s' % image_id)
+        response = requests.get(path, headers=self._headers())
+        self.assertEqual(200, response.status_code)
+        image = jsonutils.loads(response.text)
+        self.assertEqual('', image['my_empty_prop'])
+
+        self.stop_servers()
+
     def test_image_lifecycle(self):
         # Image list should be empty
         self.start_servers(**self.__dict__.copy())
