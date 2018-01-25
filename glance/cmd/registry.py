6f63d28d9b8727409fd982d5920846a942b6d43e
Erno Kuvaja <jokke@usr.fi>
2018-01-25 16:16:22 +0000

    Fix py27 eventlet issue <0.22.0
    Closes-Bug: #1744824
    
    Change-Id: Ib9f8a52136e25d1cb609d465ca5d859523d9acc6

--- glance/cmd/registry.py
+++ glance/cmd/registry.py
@@ -28,6 +28,12 @@ import eventlet
 from oslo_utils import encodeutils
 
 # Monkey patch socket and time
+# NOTE(jokke): As per the eventlet commit
+# b756447bab51046dfc6f1e0e299cc997ab343701 there's circular import happening
+# which can be solved making sure the hubs are properly and fully imported
+# before calling monkey_patch(). This is solved in eventlet 0.22.0 but we
+# need to address it before that is widely used around.
+eventlet.hubs.get_hub()
 eventlet.patcher.monkey_patch(all=False, socket=True, time=True, thread=True)
 
 # If ../glance/__init__.py exists, add ../ to Python search path, so that
--- glance/tests/__init__.py
+++ glance/tests/__init__.py
@@ -28,4 +28,10 @@ logger.addHandler(hdlr)
 logger.setLevel(logging.DEBUG)
 
 import eventlet
+# NOTE(jokke): As per the eventlet commit
+# b756447bab51046dfc6f1e0e299cc997ab343701 there's circular import happening
+# which can be solved making sure the hubs are properly and fully imported
+# before calling monkey_patch(). This is solved in eventlet 0.22.0 but we
+# need to address it before that is widely used around.
+eventlet.hubs.get_hub()
 eventlet.patcher.monkey_patch()
