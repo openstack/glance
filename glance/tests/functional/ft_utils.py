baa52224dc73e8ca3bdcbdaa5d62bc26fb38052f
Brian Rosmaita <rosmaita.fossdev@gmail.com>
2018-08-14 21:01:22 -0400

    Add multihash checks to functional tests
    Refactor the image functional tests to pull common code into a
    utility function and add multihash checks to those functional tests
    that are only checking that the legacy checksum is set correctly.
    
    Change-Id: If4e2dbb6db2bee0b8d8e307ce2f7f7bd15cddea8
    (cherry picked from commit 432b9f6c24f8c4a121767b60a0f81eb2185d1ed5)

--- glance/tests/functional/ft_utils.py
+++ glance/tests/functional/ft_utils.py
@@ -13,11 +13,39 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import six
 import time
 
 from oslo_serialization import jsonutils
 import requests
 from six.moves import http_client as http
+
+
+def verify_image_hashes_and_status(
+        test_obj, image_id, checksum=None, os_hash_value=None, status=None,
+        os_hash_algo='sha512'):
+    """Makes image-detail request and checks response.
+
+    :param test_obj: The test object; expected to have _url() and
+                     _headers() defined on it
+    :param image_id: Image id to use in the request
+    :param checksum: Expected checksum (default: None)
+    :param os_hash_value: Expected multihash value (default: None)
+    :param status: Expected status (default: None)
+    :param os_hash_algo: Expected value of os_hash_algo; only checked when
+                         os_hash_value is not None (default: 'sha512')
+    """
+    path = test_obj._url('/v2/images/%s' % image_id)
+    response = requests.get(path, headers=test_obj._headers())
+    test_obj.assertEqual(http.OK, response.status_code)
+    image = jsonutils.loads(response.text)
+    test_obj.assertEqual(checksum, image['checksum'])
+    if os_hash_value:
+        # make sure we're using the hashing_algorithm we expect
+        test_obj.assertEqual(six.text_type(os_hash_algo),
+                             image['os_hash_algo'])
+    test_obj.assertEqual(os_hash_value, image['os_hash_value'])
+    test_obj.assertEqual(status, image['status'])
 
 
 def wait_for_status(request_path, request_headers, status='active',
