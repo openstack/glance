8c3751db21133fde4e47965ac6ea9971b3352c50
Abhishek Kekane <akekane@redhat.com>
2019-03-13 18:12:42 +0000

    Data remains in staging area if 'file' store is not enabled
    When operator has not enabled 'file' store and using other stores like ceph,
    swift etc. the uploading to staging area works as we explicitly
    build 'file' store during this operation, while cleaning up we directly
    use 'glance_store.delete_from_backend' which only works if 'file'
    store is enabled.
    
    Modified '_DeleteFromFS' task and _unstage call which will use os
    module to unlink the file present in staging area explicitly to
    delete the data from staging area.
    
    Conflicts:
          glance/tests/unit/async/flows/test_web_download.py
    
    NOTE: Renamed import statement from above test files as async_
    module is not available in stable/rocky.
    
    Closes-Bug: #1803498
    Change-Id: If0b3b0af9300301291758c67267890e0959ebb3c
    (cherry picked from commit c92724608512d42b56152d199cc0fea049e4d4a7)

--- glance/tests/unit/async/flows/test_web_download.py
+++ glance/tests/unit/async/flows/test_web_download.py
@@ -14,13 +14,14 @@
 #    under the License.
 
 import mock
+import os
 
 from glance_store._drivers import filesystem
 from glance_store import backend
 from oslo_config import cfg
 
 from glance.async.flows._internal_plugins import web_download
-
+from glance.async.flows import api_image_import
 import glance.common.exception
 import glance.common.scripts.utils as script_utils
 from glance import domain
@@ -86,3 +87,50 @@ class TestWebDownloadTask(test_utils.BaseTestCase):
                               self.task_type, self.task_repo, self.image_id,
                               self.uri)
             mock_override.assert_called()
+
+    def test_web_download_failed(self):
+        web_download_task = web_download._WebDownload(
+            self.task.task_id, self.task_type, self.task_repo,
+            self.image_id, self.uri)
+        with mock.patch.object(script_utils,
+                               "get_image_data_iter") as mock_iter:
+            mock_iter.side_effect = glance.common.exception.NotFound
+            self.assertRaises(glance.common.exception.NotFound,
+                              web_download_task.execute)
+
+    def test_web_download_delete_staging_image_not_exist(self):
+        staging_path = "file:///tmp/staging/temp-image"
+        delete_from_fs_task = api_image_import._DeleteFromFS(
+            self.task.task_id, self.task_type)
+        with mock.patch.object(os.path, "exists") as mock_exists:
+            mock_exists.return_value = False
+            with mock.patch.object(os, "unlink") as mock_unlik:
+                delete_from_fs_task.execute(staging_path)
+
+                self.assertEqual(1, mock_exists.call_count)
+                self.assertEqual(0, mock_unlik.call_count)
+
+    @mock.patch.object(os.path, "exists")
+    def test_web_download_delete_staging_image_failed(self, mock_exists):
+        mock_exists.return_value = True
+        staging_path = "file:///tmp/staging/temp-image"
+        delete_from_fs_task = api_image_import._DeleteFromFS(
+            self.task.task_id, self.task_type)
+        with mock.patch.object(os, "unlink") as mock_unlink:
+            try:
+                delete_from_fs_task.execute(staging_path)
+            except OSError:
+                self.assertEqual(1, mock_unlink.call_count)
+
+            self.assertEqual(1, mock_exists.call_count)
+
+    @mock.patch.object(os.path, "exists")
+    def test_web_download_delete_staging_image_succeed(self, mock_exists):
+        mock_exists.return_value = True
+        staging_path = "file:///tmp/staging/temp-image"
+        delete_from_fs_task = api_image_import._DeleteFromFS(
+            self.task.task_id, self.task_type)
+        with mock.patch.object(os, "unlink") as mock_unlik:
+            delete_from_fs_task.execute(staging_path)
+            self.assertEqual(1, mock_exists.call_count)
+            self.assertEqual(1, mock_unlik.call_count)
