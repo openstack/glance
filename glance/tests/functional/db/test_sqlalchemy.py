2a51843138e27071bf84269f6b2a601b3ba9978f
Dan Smith <dansmith@redhat.com>
2020-06-24 13:06:38 -0700

    Add image_set_property_atomic() helper
    This adds a new DB API method to atomically create a property on an image
    in a way that we can be sure it is created once and only once for the
    purposes of exclusion of multiple threads.
    
    Change-Id: Ifdb711cb241ef13eccaa5ae29a234f2fe4a52eb8
    Related-Bug: #1884596

--- glance/tests/functional/db/test_sqlalchemy.py
+++ glance/tests/functional/db/test_sqlalchemy.py
@@ -170,3 +170,139 @@ class TestMetadefSqlAlchemyDriver(base_metadef.TestMetadefDriver,
         db_tests.load(get_db, reset_db_metadef)
         super(TestMetadefSqlAlchemyDriver, self).setUp()
         self.addCleanup(db_tests.reset)
+
+
+class TestImageAtomicUpdate(base.TestDriver,
+                            base.FunctionalInitWrapper):
+
+    def setUp(self):
+        db_tests.load(get_db, reset_db)
+        super(TestImageAtomicUpdate, self).setUp()
+
+        self.addCleanup(db_tests.reset)
+        self.image = self.db_api.image_create(
+            self.adm_context,
+            {'status': 'active',
+             'owner': self.adm_context.owner,
+             'properties': {'speed': '88mph'}})
+
+    @staticmethod
+    def _propdict(list_of_props):
+        """
+        Convert a list of ImageProperty objects to dict, ignoring
+        deleted values.
+        """
+        return {x.name: x.value
+                for x in list_of_props
+                if x.deleted == 0}
+
+    def assertOnlyImageHasProp(self, image_id, name, value):
+        images_with_prop = self.db_api.image_get_all(
+            self.adm_context,
+            {'properties': {name: value}})
+        self.assertEqual(1, len(images_with_prop))
+        self.assertEqual(image_id, images_with_prop[0]['id'])
+
+    def test_update(self):
+        """Try to double-create a property atomically.
+
+        This should ensure that a second attempt to create the property
+        atomically fails with Duplicate.
+        """
+
+        # Atomically create the property
+        self.db_api.image_set_property_atomic(self.image['id'],
+                                              'test_property', 'foo')
+
+        # Make sure only the matched image got it
+        self.assertOnlyImageHasProp(self.image['id'], 'test_property', 'foo')
+
+        # Trying again should fail
+        self.assertRaises(exception.Duplicate,
+                          self.db_api.image_set_property_atomic,
+                          self.image['id'], 'test_property', 'bar')
+
+        # Ensure that only the first one stuck
+        image = self.db_api.image_get(self.adm_context, self.image['id'])
+        self.assertEqual({'speed': '88mph', 'test_property': 'foo'},
+                         self._propdict(image['properties']))
+        self.assertOnlyImageHasProp(self.image['id'], 'test_property', 'foo')
+
+    def test_update_drop_update(self):
+        """Try to create, delete, re-create property atomically.
+
+        If we fail to undelete and claim the property, this will
+        fail as duplicate.
+        """
+
+        # Atomically create the property
+        self.db_api.image_set_property_atomic(self.image['id'],
+                                              'test_property', 'foo')
+
+        # Ensure that it stuck
+        image = self.db_api.image_get(self.adm_context, self.image['id'])
+        self.assertEqual({'speed': '88mph', 'test_property': 'foo'},
+                         self._propdict(image['properties']))
+        self.assertOnlyImageHasProp(self.image['id'], 'test_property', 'foo')
+
+        # Update the image with the property removed, like image_repo.save()
+        new_props = self._propdict(image['properties'])
+        del new_props['test_property']
+        self.db_api.image_update(self.adm_context, self.image['id'],
+                                 values={'properties': new_props},
+                                 purge_props=True)
+
+        # Make sure that a fetch shows the property deleted
+        image = self.db_api.image_get(self.adm_context, self.image['id'])
+        self.assertEqual({'speed': '88mph'},
+                         self._propdict(image['properties']))
+
+        # Atomically update the property, which still exists, but is
+        # deleted
+        self.db_api.image_set_property_atomic(self.image['id'],
+                                              'test_property', 'bar')
+
+        # Makes sure we updated the property and undeleted it
+        image = self.db_api.image_get(self.adm_context, self.image['id'])
+        self.assertEqual({'speed': '88mph', 'test_property': 'bar'},
+                         self._propdict(image['properties']))
+        self.assertOnlyImageHasProp(self.image['id'], 'test_property', 'bar')
+
+    def test_update_prop_multiple_images(self):
+        """Create and delete properties on two images, then set on one.
+
+        This tests that the resurrect-from-deleted mode of the method only
+        matchs deleted properties from our image.
+        """
+
+        images = self.db_api.image_get_all(self.adm_context)
+
+        image_id1 = images[0]['id']
+        image_id2 = images[-1]['id']
+
+        # Atomically create the property on each image
+        self.db_api.image_set_property_atomic(image_id1,
+                                              'test_property', 'foo')
+        self.db_api.image_set_property_atomic(image_id2,
+                                              'test_property', 'bar')
+
+        # Make sure they got the right property value each
+        self.assertOnlyImageHasProp(image_id1, 'test_property', 'foo')
+        self.assertOnlyImageHasProp(image_id2, 'test_property', 'bar')
+
+        # Delete the property on both images
+        self.db_api.image_update(self.adm_context, image_id1,
+                                 {'properties': {}},
+                                 purge_props=True)
+        self.db_api.image_update(self.adm_context, image_id2,
+                                 {'properties': {}},
+                                 purge_props=True)
+
+        # Set the property value on one of the images. Both will have a
+        # deleted previous value for the property, but only one should
+        # be updated
+        self.db_api.image_set_property_atomic(image_id2,
+                                              'test_property', 'baz')
+
+        # Make sure the update affected only the intended image
+        self.assertOnlyImageHasProp(image_id2, 'test_property', 'baz')
