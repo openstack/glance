173fdb1968f7bc106040bd6beb3a02663a6138cd
Dan Smith <dansmith@redhat.com>
2020-08-18 07:51:43 -0700

    Add FakeData generator test utility
    This adds a FakeData file-like generator that is able to generate
    arbitrary amounts of data when we need to simulate reading from a
    file or request pipeline, without having to store all of that in
    memory.
    
    Change-Id: Iff1fbe2b55f4be12e69c9fd3dec7e3b3e2593e53
    (cherry picked from commit dc08127f059a227928400bf2b629ef503dbf28e7)

--- glance/tests/utils.py
+++ glance/tests/utils.py
@@ -32,6 +32,7 @@ from oslo_config import cfg
 from oslo_config import fixture as cfg_fixture
 from oslo_log.fixture import logging_error as log_fixture
 from oslo_log import log
+from oslo_utils import units
 import six
 from six.moves import BaseHTTPServer
 from six.moves import http_client as http
@@ -690,3 +691,44 @@ def start_standalone_http_server():
     thread.start()
 
     return thread, httpd, port
+
+
+class FakeData(object):
+    """Generate a bunch of data without storing it in memory.
+
+    This acts like a read-only file object which generates fake data
+    in chunks when read() is called or it is used as a generator. It
+    can generate an arbitrary amount of data without storing it in
+    memory.
+
+    :param length: The number of bytes to generate
+    :param chunk_size: The chunk size to return in iteration mode, or when
+                       read() is called unbounded
+
+    """
+    def __init__(self, length, chunk_size=64 * units.Ki):
+        self._max = length
+        self._chunk_size = chunk_size
+        self._len = 0
+
+    def read(self, length=None):
+        if length is None:
+            length = self._chunk_size
+
+        length = min(length, self._max - self._len)
+
+        self._len += length
+        if length == 0:
+            return b''
+        else:
+            return b'0' * length
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        r = self.read()
+        if len(r) == 0:
+            raise StopIteration()
+        else:
+            return r
