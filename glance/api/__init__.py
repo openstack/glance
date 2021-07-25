7ab90017503807f5dc6e46a8f97cb24d566264d3
Takashi Kajinami <tkajinam@redhat.com>
2021-07-25 15:20:40 +0900

    Refactor pipeline definition
    This introduces a new pipeline_factory so that we can use a single
    entry to define pipeline for each deployment flavor.
    
    Change-Id: Ia9b251f0e48f69baa0c5c988b91b216a627ed028

--- glance/api/__init__.py
+++ glance/api/__init__.py
@@ -21,3 +21,15 @@ CONF = cfg.CONF
 
 def root_app_factory(loader, global_conf, **local_conf):
     return paste.urlmap.urlmap_factory(loader, global_conf, **local_conf)
+
+
+def pipeline_factory(loader, global_conf, **local_conf):
+    """A paste pipeline replica that keys off of deployment flavor."""
+    pipeline = local_conf[CONF.paste_deploy.flavor or 'default']
+    pipeline = pipeline.split()
+    filters = [loader.get_filter(n) for n in pipeline[:-1]]
+    app = loader.get_app(pipeline[-1])
+    filters.reverse()
+    for filter in filters:
+        app = filter(app)
+    return app
