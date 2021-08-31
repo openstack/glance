07eaf532c7e0e8bdfdca5c03ffcc549e1793c2be
Lance Bragstad <lbragstad@gmail.com>
2021-08-31 20:05:43 +0000

    Remove duplicate _header() implementations in metadef tests
    The metadef functional test classes all had their own _header()
    implementation and none of them varied. This commit moves the _header()
    implementation to a common layer and removes the duplication.
    
    Change-Id: I40942a5dde3cd50a64a5f36fbcb6d9240af25b5e

--- glance/tests/functional/v2/metadef_base.py
+++ glance/tests/functional/v2/metadef_base.py
@@ -25,6 +25,17 @@ class MetadefFunctionalTestBase(functional.FunctionalTest):
         self.tenant1 = uuids.owner1
         self.tenant2 = uuids.owner2
 
+    def _headers(self, custom_headers=None):
+        base_headers = {
+            'X-Identity-Status': 'Confirmed',
+            'X-Auth-Token': '932c5c84-02ac-4fe5-a9ba-620af0e2bb96',
+            'X-User-Id': 'f9a41d13-0c13-47e9-bee2-ce4e8bfe958e',
+            'X-Tenant-Id': self.tenant1,
+            'X-Roles': 'admin',
+        }
+        base_headers.update(custom_headers or {})
+        return base_headers
+
     def assertNamespacesEqual(self, actual, expected):
         """Assert two namespace dictionaries are the same."""
         actual.pop('created_at', None)
