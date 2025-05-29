e055873be4079bc9d37165cf75de5495603133b0
Brian Rosmaita <rosmaita.fossdev@gmail.com>
2025-05-29 19:21:23 -0400

    Get ready for oslo.limit 2.7.0
    Change Ic4857f555352 into olso.limit changes the way limits are
    determined and breaks our custom KeystoneQuotaFixture [0].  Make this
    fixture less customized by making it a subclass of olso.limit's
    LimitFixture (instead of a subclass of python fixtures) so we can
    inherit changes to the test fixture.
    
    [0] See test results on
        https://review.opendev.org/c/openstack/requirements/+/951104
    
    Change-Id: Iaee4f7eb2b49d4d907e34d90d18a26b4d2323215

--- glance/tests/unit/fixtures.py
+++ glance/tests/unit/fixtures.py
@@ -15,14 +15,11 @@
 
 import logging as std_logging
 import os
-from unittest import mock
 import warnings
 
 import fixtures as pyfixtures
-from openstack.identity.v3 import endpoint
-from openstack.identity.v3 import limit as klimit
 from oslo_db import warning as oslo_db_warning
-from oslo_limit import limit
+from oslo_limit import fixture as ol_fixture
 from sqlalchemy import exc as sqla_exc
 
 _TRUE_VALUES = ('True', 'true', '1', 'yes')
@@ -191,30 +188,6 @@ class WarningsFixture(pyfixtures.Fixture):
         warnings.filters[:] = self._original_warning_filters
 
 
-class KeystoneQuotaFixture(pyfixtures.Fixture):
+class KeystoneQuotaFixture(ol_fixture.LimitFixture):
     def __init__(self, **defaults):
-        self.defaults = defaults
-
-    def setUp(self):
-        super(KeystoneQuotaFixture, self).setUp()
-
-        self.mock_conn = mock.MagicMock()
-        limit._SDK_CONNECTION = self.mock_conn
-
-        mock_gem = self.useFixture(
-            pyfixtures.MockPatch('oslo_limit.limit.Enforcer.'
-                                 '_get_enforcement_model')).mock
-        mock_gem.return_value = 'flat'
-
-        fake_endpoint = endpoint.Endpoint()
-        fake_endpoint.service_id = "service_id"
-        fake_endpoint.region_id = "region_id"
-        self.mock_conn.get_endpoint.return_value = fake_endpoint
-
-        def fake_limits(service_id, region_id, resource_name, project_id):
-            this_limit = klimit.Limit()
-            this_limit.resource_name = resource_name
-            this_limit.resource_limit = self.defaults[resource_name]
-            return iter([this_limit])
-
-        self.mock_conn.limits.side_effect = fake_limits
+        super(KeystoneQuotaFixture, self).__init__(defaults, {})
